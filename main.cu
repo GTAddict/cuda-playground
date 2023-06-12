7800b42351c547b46d159d8b89f8b768ce4f9a4f
GTAddict <soregtaddict@gmail.com>
2023-06-11 23:53:14 -0700

    Update main.cu
    Okay, allocating one block of memory and then striding x y and z definitely gives much much better performance (CUDA Test B). I'm not happy though because something about the thread calculation is making larger numbers of elements finish way faster than smaller numbers of elements. It's so fast in fact that it makes me thing something is wrong. Need to check.

--- main.cu
+++ main.cu
@@ -8,64 +8,110 @@
 using namespace std::chrono;
 
 __global__
-void init_curand(curandState* state, unsigned long seed, int num_elements)
+void init_curand(curandState* state, int numElements)
 {
 	const int id = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if (id < num_elements)
+	if (id < numElements)
 	{
-		curand_init(seed, id, 0, &state[id]);
+		curand_init(id, 0, 0, &state[id]);
 	}
 }
 
 __global__
-void randomize(curandState* state, float* a, int num_elements)
+void randomize(curandState* state, float* a, int numElements)
 {
 	const int id = blockIdx.x * blockDim.x + threadIdx.x;
-	if (id < num_elements)
+	if (id < numElements)
 	{
 		a[id] = curand_uniform(state + id);
 	}
 }
 
 __global__
-void add(float* a, float* b, float* out, int num_elements)
+void add(float* a, float* b, float* out, int numElements)
 {
 	const int id = blockIdx.x * blockDim.x + threadIdx.x;
-	if (id < num_elements)
+	if (id < numElements)
 	{
 		out[id] = a[id] + b[id];
 	}
 }
 
-auto runCudaTest(float num_elements)
+auto runCudaTestA(int num_elements)
 {
 	auto test_begin = high_resolution_clock::now();
 
 	float* x, * y, * z;
-	cudaMallocManaged(&x, num_elements * sizeof(float));
-	cudaMallocManaged(&y, num_elements * sizeof(float));
-	cudaMallocManaged(&z, num_elements * sizeof(float));
+	cudaMalloc(&x, num_elements * sizeof(float));
+	cudaMalloc(&y, num_elements * sizeof(float));
+	cudaMalloc(&z, num_elements * sizeof(float));
 
 	constexpr int blockSize = 256;
 	int numBlocks = ((num_elements - 1) / blockSize) + 1;
 	curandState* randomStates;
-	cudaMalloc(&randomStates, sizeof(curandState) * num_elements);
+	cudaMalloc(&randomStates, sizeof(curandState) * num_elements * 2);
 
-	init_curand<<<numBlocks, blockSize >> > (randomStates, 0, num_elements);
+	init_curand<<<numBlocks, blockSize>>> (randomStates, num_elements);
 	cudaDeviceSynchronize();
-
 	randomize<<<numBlocks, blockSize>>> (randomStates, x, num_elements);
-	randomize<<<numBlocks, blockSize>>>(randomStates, y, num_elements);
+	randomize<<<numBlocks, blockSize>>>(randomStates + num_elements, y, num_elements);
 	cudaDeviceSynchronize();
-
 	add<<<numBlocks, blockSize >> > (x, y, z, num_elements);
 	cudaDeviceSynchronize();
-
 	cudaFree(randomStates);
 	cudaFree(x);
 	cudaFree(y);
 	cudaFree(z);
+
+	auto test_end = high_resolution_clock::now();
+
+	return test_end - test_begin;
+}
+
+struct collection
+{
+	float x, y, z;
+};
+
+__global__
+void randomize_b(collection* collections, int num_elements)
+{
+	curandState state;
+	const int id = blockIdx.x * blockDim.x + threadIdx.x;
+	if (id < num_elements)
+	{
+		curand_init(id, 0, 0, &state);
+		collections[id].x = curand_uniform(&state);
+		collections[id].y = curand_uniform(&state);
+	}
+}
+
+__global__
+void add_b(collection* collections, int num_elements)
+{
+	const int id = blockIdx.x * blockDim.x + threadIdx.x;
+	if (id < num_elements)
+	{
+		collections[id].z = collections[id].x + collections[id].y;
+	}
+}
+
+auto runCudaTestB(int num_elements)
+{
+	auto test_begin = high_resolution_clock::now();
+
+	collection* collections;
+	cudaMalloc(&collections, num_elements * sizeof(collection));
+
+	constexpr int blockSize = 256;
+	int numBlocks = ((num_elements - 1) / blockSize) + 1;
+	randomize_b<<<numBlocks, blockSize>>>(collections, num_elements);
+	cudaDeviceSynchronize();
+	add_b<<<numBlocks, blockSize>>>(collections, num_elements);
+	cudaDeviceSynchronize();
+
+	cudaFree(collections);
 
 	auto test_end = high_resolution_clock::now();
 
@@ -116,10 +162,11 @@ auto runCPUTest(int num_elements)
 
 int main()
 {
-	constexpr int num_elements = 1 << 29;
+	constexpr int num_elements = 1 << 25;
 
 	std::cout << "Num elements: " << num_elements << std::endl;
-	std::cout << "CUDA test: " << duration<float, milliseconds::period>(runCudaTest(num_elements)).count() << std::endl;
+	std::cout << "CUDA test A: " << duration<float, milliseconds::period>(runCudaTestA(num_elements)).count() << std::endl;
+	std::cout << "CUDA test B: " << duration<float, milliseconds::period>(runCudaTestB(num_elements)).count() << std::endl;
 	std::cout << "CPU test: " << duration<float, milliseconds::period>(runCPUTest(num_elements)).count() << std::endl;
 	// Will add SIMD test later...
 }
